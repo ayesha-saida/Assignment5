3ba854e31ffdc708c3e63e1d6335c3c08bb27b32
ayesha-saida <ayeshasultana0305@gmail.com>
2025-08-29 04:24:29 +0600

    call & coin button is working

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ getElement('card-items').addEventListener('click', function (e) {
     // alert('card clicked');
   }
 });
+
 //heart number increment
 let iconNumber = document.querySelectorAll('.icon-btn');
 for (const icon of iconNumber) {
@@ -16,3 +17,53 @@ for (const icon of iconNumber) {
     document.getElementById('count-number').innerHTML = addNumber;
   });
 }
+
+//call button & coins reduction
+const callbtns = document.getElementsByClassName('call-btn');
+//console.log(callbtns);
+let coinAmount = document.getElementById('coin-number');
+//history container
+const historyContainer = getElement('history-container');
+const history = document.createElement('div');
+history.className =
+  ' flex justify-between items-center bg-gray-200 rounded-[10px]';
+
+//live time
+const now = new Date();
+const localTime = now.toLocaleTimeString();
+
+for (let call of callbtns) {
+  call.addEventListener('click', function () {
+    const serviceName = call.parentNode.parentNode.childNodes[3].innerText;
+    //console.log(serviceName);
+    const serviceNumber = call.parentNode.parentNode.childNodes[7].innerText;
+    //console.log(serviceNumber);
+
+    alert('Calling' + ' ' + serviceName + ':' + serviceNumber + '...');
+
+    history.innerHTML = `
+          <div class="flex flex-col">
+           <h1 class="mx-5 text-center">${serviceName}
+          </h1>
+            <h1 class="mt-3 mx-5">${serviceNumber}</h1>
+          </div>
+          <span class="text-sm mx-2 text-center block">${localTime}</span>`;
+    historyContainer.appendChild(history);
+
+    //coin reduce
+    const currentCoin = Number(coinAmount.innerText);
+    if (currentCoin === 0) {
+      alert('Your coin is finished. Atleast 20 coins needed for a call.');
+      callbtns.disabled = true;
+    } else {
+      const coinReduce = Number(currentCoin) - Number(20);
+      coinAmount.innerHTML = coinReduce;
+    }
+  });
+}
+
+//clear history
+getElement('clear-btn').addEventListener('click', function () {
+  const history_Container = getElement('history-container');
+  history_Container.innerHTML = '';
+});
